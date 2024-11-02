6478e3f783185bd9fea5f8d1b1a43b52ddf443c9
user.name <avi26022007@gmail.com>
2024-11-02 16:02:06 +0530

    sign up page

--- Client_side/src/App.jsx
+++ Client_side/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FullPage from './sections/fullPage'; 
+import SignUp from './sections/SignUp';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<FullPage />} />
-       
-      
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </Router>
   );
--- Client_side/src/sections/SignUp.jsx
+++ Client_side/src/sections/SignUp.jsx
@@ -2,7 +2,18 @@
 
 const SignUp = () => {
   return (
-    <div></div>
+   
+    <div className="flex h-screen justify-center items-center "  >     
+      <form method='post' action="">
+        <div className="flex flex-col shadow-2xl bg-slate-50 gap-2 border-[2px]  border-coral-red pt-32  pb-32 pl-24 pr-24 rounded-lg ">
+        <h1 className="font-bold text-coral-red text-3xl">Enter your details to SignUp</h1>
+        <input className="flex border-[3px] bg-slate-100 border-coral-red rounded-sm  p-3 w-full " type="text" placeholder="Name" name="name" />
+        <input className="flex border-[3px] bg-slate-100 border-coral-red rounded-sm  p-3 w-full " type="email" placeholder="E-mail" name="email" /> 
+        <input className="flex border-[3px] bg-slate-100 border-coral-red rounded-sm  p-3 w-full" type="password" placeholder="Password" name="password" />
+        </div>
+
+      </form>
+    </div>
   )
 }
 
